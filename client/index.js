0c591979cffe09a59bac27792d5eb29f14dbffd1
NakamotoNomad <nakamotonomad@outlook.com>
2023-10-31 17:50:19 +0700

    Solved exercise of Module 2
    - The client generates the Merkle Tree and proof and passes those to the server
    - The server verifies the proof and returns if that name is included or not

--- client/index.js
+++ client/index.js
@@ -4,11 +4,50 @@ const MerkleTree = require('../utils/MerkleTree');
 
 const serverUrl = 'http://localhost:1225';
 
+/*
+Usage:
+Call `node client/index` from the root of the project to run it without arguments.
+Then it'll default to check the name "Brad Reinger", a valid name.
+
+--name <name> Pass the name to be checked
+--verbose     Enable verbose output
+
+Examples:
+$ node client/index --name "Phil Murray"
+$ node client/index --name "Spongebob Squarepants" --verbose
+ */
+
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  // let myName = "NakamotoNomad"; // invalid example
+  let myName = "Brad Reinger"; // valid example
+  let verbose = false;
+
+  const args = process.argv.slice(2);
+  for (let i = 0; i < args.length; i++) {
+    if (args[i] === '--name') {
+      myName = args[i + 1];
+    } else if (args[i] === '--verbose') {
+      verbose = true;
+    }
+  }
+
+  const merkleTree = new MerkleTree(niceList);
+  const root = merkleTree.getRoot();
+
+  if (verbose) {
+    console.log("Created merkle tree with root: " + root);
+  }
+
+  const index = niceList.findIndex(n => n === myName);
+  const myProof = merkleTree.getProof(index);
+
+  if (verbose) {
+    console.log("Created proof: " + JSON.stringify(myProof));
+  }
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    name: myName,
+    proof: myProof
   });
 
   console.log({ gift });
--- server/index.js
+++ server/index.js
@@ -6,20 +6,19 @@ const port = 1225;
 const app = express();
 app.use(express.json());
 
-// TODO: hardcode a merkle root here representing the whole nice list
-// paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const MERKLE_ROOT = 'ddd59a2ffccddd60ff47993312821cd57cf30f7f14fb82937ebe2c4dc78375aa'; // created based on niceList
 
 app.post('/gift', (req, res) => {
-  // grab the parameters from the front-end here
   const body = req.body;
 
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
-  if(isInTheList) {
+  console.log("Checking gift. Request body: " + JSON.stringify(body));
+
+  const { name, proof } = body;
+
+  const isInTheList = verifyProof(proof, name, MERKLE_ROOT);
+  if (isInTheList) {
     res.send("You got a toy robot!");
-  }
-  else {
+  } else {
     res.send("You are not on the list :(");
   }
 });
